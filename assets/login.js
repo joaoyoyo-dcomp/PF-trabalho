064eeeaa5585633dcb0e47fde0a91552f8d209da
joaoyoyo-dcomp <joao.yoyo@dcomp.ufs.br>
2025-08-19 10:54:08 -0300

    Funções adicionadas para validar e testar a entrada de um nome de usuário na página de login. isValid verifica se algo foi digitado, buttonState reconhece se algo válido foi digitado (no caso, qualquer palavra) e loginState irá criar o estado de login

--- assets/login.js
+++ assets/login.js
@@ -1,3 +1,9 @@
 const input = document.querySelector('.login__input');
 const button = document.querySelector('.login__button');
 const form = document.querySelector('.login-form');
+
+const isValid = value => value.length >= 1;
+const buttonState = valid => valid ? { disabled: false } : { disabled: true };
+
+const loginState = value => isValid(value) ? { player: value } : null;
+
